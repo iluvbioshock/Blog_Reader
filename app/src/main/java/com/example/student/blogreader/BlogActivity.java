2e9c5fd7ee990b51d26cd426fd21664d7c519e87
Krystal Scott <iluvhugs4evr@gmail.com>
2014-11-05 20:47:51 -0800

    commit 5

---
+++ app/src/main/java/com/example/student/blogreader/BlogActivity.java
@@ -0,0 +1,31 @@
+
+
+
+package com.example.student.blogreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+
+public class BlogActivity extends Activity {
+    protected ProgressBar progressBar;
+    protected ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_reader);
+        progressBar =(ProgressBar)findViewById(R.id.progressBar):
+        listView =(ListView)findViewById(R.id.listView);
+        listView.setEmptyView(progressBar);
+
+    }
+    }
+
+
+    
