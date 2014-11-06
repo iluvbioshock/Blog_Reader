e4eb1472cdef75b67993387a8cb9b8c5e2f55119
Krystal Scott <iluvhugs4evr@gmail.com>
2014-11-05 22:40:24 -0800

    commit 10

--- app/src/main/java/com/example/student/blogreader/BlogPostTask.java
+++ app/src/main/java/com/example/student/blogreader/BlogPostTask.java
@@ -6,6 +6,9 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -14,9 +17,19 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     protected JSONObject doInBackground(Activity... params){
         try {
             URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=10");
-        }
+
+            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+            connection.connect();
+            int responseCode= connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK){
+                
+            }
         catch (MalformedURLException error){
             Log.e("BlogPostTask", "MalformedURLException" + error);
+        }
+        catch (IOException error){
+            Log.e("BlogPostTask", "IOException" + error);
         }
         return null;
     }
